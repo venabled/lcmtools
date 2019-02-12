2724078ea6a63aeec22acd8660bca1ee3f0dbda1
Donald Venable <venabled@gmail.com>
2019-02-12 12:28:17 -0500

    Refactored into module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='lcmtools',
+    version='0.1',
+    packages=['lcmtools'],
+    url='github.com/venabled/lcmtools',
+    license='MIT',
+    author='Donald Venable',
+    author_email='venabled@gmail.com',
+    description='Tools for interacting with LCM logfiles'
+)
